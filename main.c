b1c4b48c6c2de3219e4ce4c0d4959cff4648a7b8
Tobias Quinn <tobias@tobiasquinn.com>
2014-04-30 04:04:47 +0100

    turn off and restore keyboard backlight at ten second idle

--- main.c
+++ main.c
@@ -4,18 +4,20 @@
 #define GNOME_DESKTOP_USE_UNSTABLE_API 1
 #include <gnome-idle-monitor.h>
 
-#define DELAY_TIME 1000
+#define DELAY_TIME 10000
 #define BACKLIGHT_SYS_FILE "/sys/class/leds/smc::kbd_backlight/brightness"
 #define BACKLIGHT_SUDO_COMMAND "echo %d | sudo tee %s\n"
 
-//static int existing_backlight_value = -1;
-int backlight_fp; // backlight control file
+static int saved_backlight_value = -1;
+//int backlight_fp; // backlight control file
+GnomeIdleMonitor *gim; // idle monitor instance
 
 #define MAX_LINE 255
 
 void set_backlight_value(int value) {
     char line[MAX_LINE];
     sprintf(line, BACKLIGHT_SUDO_COMMAND, value, BACKLIGHT_SYS_FILE);
+    printf("VAL %d LINE:%s\n", value, line);
     system(line);
 }
 
@@ -24,36 +26,49 @@ int get_backlight_value(void) {
     int n;
     char line[MAX_LINE];
 
+    // open the backlight control file
+    int backlight_fp = open(BACKLIGHT_SYS_FILE, O_RDONLY);
     n = read(backlight_fp, line, MAX_LINE);
     if (n == -1)
         printf("File read error\n");
     n = atoi(line);
+    close(backlight_fp);
     return n;
 }
 
-void watch_func(GnomeIdleMonitor *monitor,
+void active_watch_func(GnomeIdleMonitor *monitor,
         guint id,
         gpointer user_data) {
-    printf("watch_func\r\n");
+    printf("user active\n");
+    // restore the backlight
+    set_backlight_value(saved_backlight_value);
+}
+
+void idle_watch_func(GnomeIdleMonitor *monitor,
+        guint id,
+        gpointer user_data) {
+    printf("user idle\n");
     // we want to save the existing backlight value
-    int val = get_backlight_value();
-    printf("saving backlight value %d\n", val);
+    saved_backlight_value = get_backlight_value();
+    printf("saved backlight value %d\n", saved_backlight_value);
     // and turn it down to zero
     set_backlight_value(0);
+    gnome_idle_monitor_add_user_active_watch(gim,
+            &active_watch_func,
+            0,
+            0);
 }
 
 int main(void) {
-    printf("Starting idle time detect\r\n");
+    printf("Starting idle time detect\n");
 
-    // open the backlight control file
-    backlight_fp = open(BACKLIGHT_SYS_FILE, O_RDONLY);
     GMainLoop *loop;
 
-    GnomeIdleMonitor *gim = gnome_idle_monitor_new();
+    gim = gnome_idle_monitor_new();
 
     gnome_idle_monitor_add_idle_watch(gim,
             DELAY_TIME,
-            &watch_func,
+            &idle_watch_func,
             0,
             0);
 
